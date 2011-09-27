896c1f9392255bef033f0b18b879acd56e9f2dd0
shlomo.zippel <shlomo@zigfu.com>
2011-09-26 23:10:51 -0700

    missing materials/etc

---
+++ Assets/Scripts/EnableOnHandpoint.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnableOnHandpoint : MonoBehaviour {
+
+    public Transform[] targets;
+
+	// Use this for initialization
+	void Start () {
+        Hand_Destroy();
+	}
+	
+    void Hand_Create(Vector3 pos)
+    {
+        foreach (Transform t in targets) {
+            t.gameObject.SetActiveRecursively(true);
+        }
+    }
+
+    void Hand_Destroy()
+    {
+        foreach (Transform t in targets) {
+            t.gameObject.SetActiveRecursively(false);
+        }
+    }
+}
