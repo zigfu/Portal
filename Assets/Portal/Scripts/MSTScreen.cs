0a39590f5beefa6a7877b5331a7f73562e59a87c
shlomo.zippel <shlomo@zigfu.com>
2011-09-30 19:20:37 -0700

    MSTScreen

---
+++ Assets/Portal/Scripts/MSTScreen.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class MSTScreen : MonoBehaviour {
+	
+	public string Title;
+	public Material Icon;
+	public Camera Cam;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
