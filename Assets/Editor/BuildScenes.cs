3327803ebf2c405f715040098c5bf31a01f5b98b
Roee Shenberg <roee@zigfu.com>
2011-10-10 12:45:32 -0700

    build portal script

---
+++ Assets/Editor/BuildScenes.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Linq;
+
+public class BuildScene {
+    static string scene = "Assets/Portal/_Portal.unity";
+    static string outputPath = "BuildOutput";
+	
+    [MenuItem("Build/Build Portal Executable")]
+    static void Build()
+    {   
+		if (!Directory.Exists(outputPath)) {
+			Directory.CreateDirectory(outputPath);
+		}
+		
+		PlayerSettings.displayResolutionDialog = ResolutionDialogSetting.HiddenByDefault;
+		PlayerSettings.defaultIsFullScreen = true;
+		PlayerSettings.defaultWebScreenWidth = 1280;
+		PlayerSettings.defaultScreenHeight = 720;
+		//PlayerSettings.defaultWebScreenWidth = 1024;
+		//PlayerSettings.defaultScreenHeight = 768;
+		
+		string sceneName = Path.GetFileNameWithoutExtension(scene);
+		Debug.Log("About to build " + sceneName);
+		PlayerSettings.companyName = "ZigFu";
+		PlayerSettings.productName = "Portal";
+        string res = BuildPipeline.BuildPlayer(new string[] { scene }, getOutputPath("Portal"), BuildTarget.StandaloneWindows, BuildOptions.None);
+        Debug.Log("result: " + res);
+    }
+
+    private static string getOutputPath(string scene)
+    {
+        return string.Format("{0}/bin/{1}.exe", outputPath, scene);
+    }
+}
