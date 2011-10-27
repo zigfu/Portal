7ce71e1e5ef915783d471495671b45fcc3c9b3a9
shlomo.zippel <shlomo@zigfu.com>
2011-10-27 16:59:32 -0700

    doesn't always quit

--- Assets/Portal/Scripts/OpenNIErrorDialog.cs
+++ Assets/Portal/Scripts/OpenNIErrorDialog.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 using System.Collections;
 
 public class OpenNIErrorDialog : MonoBehaviour {
-	
+	public bool CheckForOpenNIError = true;
 	public ErrorHandler handler;
 	
 	// Use this for initialization
 	void Start () {
 		Navigator nav = GetComponent<Navigator>();
 		
-		if (OpenNIContext.Instance.error) {
+		if (CheckForOpenNIError && OpenNIContext.Instance.error) {
 			handler.SetErrorText(OpenNIContext.Instance.errorMsg);
 			nav.NavigateTo(handler.transform);
 			return;
