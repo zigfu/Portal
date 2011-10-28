bd537ed85462169ad89c7ad7fd333f6f778a9545
Roee Shenberg <roee@zigfu.com>
2011-10-27 17:10:46 -0700

    MST now supports modal dialogs and sends MST_Activate/Deactivate messages

--- Assets/Portal/Scripts/MenuSystemThingie.cs
+++ Assets/Portal/Scripts/MenuSystemThingie.cs
@@ -12,21 +12,61 @@ public class MenuSystemThingie : MonoBehaviour {
 		icon.GetComponent<Renderer>().material = matIcon;
 		myCamera.gameObject.SetActiveRecursively(true); // animate
 	}
-
+    Transform lastActivatedNonModal = null;
+    Transform modalDialog = null;
 	void Navigator_ActivatedItem(Transform target)
 	{
-		MSTScreen info = target.gameObject.GetComponent<MSTScreen>();
-		if (null != info)
-		{
-			Set(info.Title, info.Icon);
-			info.Cam.rect = new Rect(0f, 0.0f, 1.0f, 0.7f); //animate
-		}
-		else 
-		{
-			myCamera.gameObject.SetActiveRecursively(false); //animate
-		}
-	}
-	
+        // if navigating away from modal dialog
+        if ((inModalDialog) && (target != modalDialog)) {
+            if (target != lastActivatedNonModal) {
+                lastActivatedNonModal.SendMessage("MST_Deactivate", SendMessageOptions.DontRequireReceiver);
+            }
+            inModalDialog = false;
+        }
+
+        // if not navigating back to the still active control
+        if (target != lastActivatedNonModal) {
+            target.SendMessage("MST_Activate", SendMessageOptions.DontRequireReceiver);
+        }
+
+        // if not navigating to modal dialog
+        if ((!inModalDialog) || (target != modalDialog)) {
+            lastActivatedNonModal = target;
+            MSTScreen info = target.gameObject.GetComponent<MSTScreen>();
+            if (null != info) {
+                Set(info.Title, info.Icon);
+                info.Cam.rect = new Rect(0f, 0.0f, 1.0f, 0.7f); //animate
+            }
+            else {
+                myCamera.gameObject.SetActiveRecursively(false); //animate
+            }
+        }
+
+    }
+
+    void Navigator_DeactivatedItem(Transform target)
+    {
+        if ((!inModalDialog)||(target != lastActivatedNonModal)) {
+            target.SendMessage("MST_Deactivate", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    private bool inModalDialog = false;
+
+    public void ShowModalDialog(Transform target)
+    {
+        if (inModalDialog) {
+            throw new System.Exception("Trying to show a modal dialog from within one. Unsupported :(");
+        }
+        inModalDialog = true;
+        modalDialog = target;
+
+        //TODO: not have implicit reference to navigator?
+        //TODO: review with teh shlomonator
+        GetComponent<Navigator>().NavigateTo(target);
+        //GetComponent<Navigator>().NavigateTo(target, false);
+    }
+
 	void OnGUI()
 	{
 		if (Event.current.Equals(Event.KeyboardEvent("escape"))) {
